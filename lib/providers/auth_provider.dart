93c4fca081bc3b85e6c0de45c50246583e20bb79
GMagan <guilherme_magan@hotmail.com>
2024-12-18 16:24:14 -0300

    fix(login): arrumando a passagem de token

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -5,9 +5,11 @@ import 'package:http/http.dart' as http;
 class AuthProvider with ChangeNotifier {
   bool _isLoggedIn = false;
   String? _currentUser;
+  String? _authToken;
 
   bool get isLoggedIn => _isLoggedIn;
   String? get currentUser => _currentUser;
+  String? get authToken => _authToken;
 
   Future<bool> login(String email, String password) async {
     try {
@@ -21,27 +23,30 @@ class AuthProvider with ChangeNotifier {
       );
 
       if (response.statusCode == 200) {
-        // Verifica se o token existe no corpo da resposta
         final String? token = jsonDecode(response.body)['access_token'];
 
         if (token != null) {
           _isLoggedIn = true;
           _currentUser = email;
+          _authToken = token;
           notifyListeners();
           return true;
         }
       } else {
         debugPrint('Login failed: ${response.statusCode} ${response.body}');
+        return false;
       }
-      return false;
     } catch (e) {
       debugPrint('Erro de login: $e');
       return false;
     }
+    return false;
   }
+
   void logout() {
     _isLoggedIn = false;
     _currentUser = null;
     notifyListeners();
   }
 }
+
